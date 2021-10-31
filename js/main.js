887c9e517f55c291753bb67a50716ee9c36665ad
khouloud <khoulouddamoun@gmail.com>
2021-10-31 21:38:20 +0100

    testimonials

--- js/main.js
+++ js/main.js
@@ -33,8 +33,28 @@ window.addEventListener('scroll', scrollNavbar)
 
 
 $(document).ready(function(){
-/*==================== CAROUSEL ====================*/
+/*==================== SERVICES CAROUSEL ====================*/
 $('.services-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+
+        }
+    }
+});
+
+/*==================== TESTIMONIALS CAROUSEL ====================*/
+$('.testimonials-carousel').owlCarousel({
     loop:true,
     margin:0,
     autoplay:true,
