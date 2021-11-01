f700b9c947e7b8cd12a2f0ba51785318bf5dbad3
khouloud <khoulouddamoun@gmail.com>
2021-11-01 15:09:59 +0100

    team

--- js/main.js
+++ js/main.js
@@ -72,4 +72,25 @@ $('.testimonials-carousel').owlCarousel({
         }
     }
 });
+
+/*==================== TEAM CAROUSEL ====================*/
+$('.team-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:4,
+
+        }
+    }
+});
+
 });
